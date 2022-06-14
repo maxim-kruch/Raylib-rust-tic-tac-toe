bd83fa18dbb8678cbbde32df6477ac5f337b4bc7
Maxim Kruch <maxim@Maxims-Air.fritz.box>
2022-06-14 18:33:46 +0200

    Fixed some bad programming

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,15 @@ fn main() {
     field = reset_game(field);
 
     rl.set_target_fps(60);
-    
+
     while !rl.window_should_close() {
-        
+
         let mut d = rl.begin_drawing(&thread);
-    
-        if d.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {        	
+
+        if d.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {
             let c_field: i32 = get_field(&d);
         	println!("Pressed Field: {}", c_field);
-        	
+
         	if c_field != 0 {
         		if c_field == 1 && field[0][0] == 0 {field[0][0] = turn;}
         		else if c_field == 2 && field[1][0] == 0 {field[1][0] = turn;}
@@ -37,16 +37,18 @@ fn main() {
         		else if c_field == 7 && field[0][2] == 0 {field[0][2] = turn;}
         		else if c_field == 8 && field[1][2] == 0 {field[1][2] = turn;}
         		else if c_field == 9 && field[2][2] == 0 {field[2][2] = turn;}
-        		
+
         		if turn == 1 {turn = 2;} else {turn = 1;}
-        		
-        		for i in 0..3 {
+
+                /*
+                for i in 0..3 {
     				for j in 0..3 {
     					print!("{}", field[i][j]);
     				}
     			}
 
                 print!("\n");
+                */
 
     			if check_winner(field) != 0 {
     				if check_winner(field) == 1 {
@@ -61,13 +63,13 @@ fn main() {
         			field = reset_game(field);
                     turn = 1;
         		}
-        	}      	
+        	}
         }
-        
-        
+
+
 
         d.clear_background(Color::RAYWHITE);
-            
+
         for i in  0..(SCREEN_WIDTH/SQUARE_SIZE + 1) {
             d.draw_line(SQUARE_SIZE*i, SQUARE_SIZE, SQUARE_SIZE*i, SCREEN_HEIGHT, Color::DARKGRAY);
         }
@@ -75,24 +77,18 @@ fn main() {
         for i in  0..(SCREEN_HEIGHT/SQUARE_SIZE + 1) {
             d.draw_line(0, 200 * (i + 1), SCREEN_WIDTH, SQUARE_SIZE * (i + 1), Color::DARKGRAY);
         }
-            
-            
-            
-        let t1 = format!("Player {}'s turn", turn);
-        let t2 = format!("Player 1: {}", p1_points);
-        let t3 = format!("Player 2: {}", p2_points);
-           
-        d.draw_text(t1.as_str(), 5, 5, 40, Color::DARKGRAY);
-        d.draw_text(t2.as_str(), 5, 50, 40, Color::DARKGRAY);
-        d.draw_text(t3.as_str(), 5, 95, 40, Color::DARKGRAY);
-            
+
+        d.draw_text(format!("Player {}'s turn", turn).as_str(), 5, 5, 40, Color::DARKGRAY);
+        d.draw_text(format!("Player 1: {}", p1_points).as_str(), 5, 50, 40, Color::DARKGRAY);
+        d.draw_text(format!("Player 2: {}", p2_points).as_str(), 5, 95, 40, Color::DARKGRAY);
+
         for i in 0..3 {
           	for j in 0..3 {
            		if field[i][j] == 1 {d.draw_circle(i as i32 * SQUARE_SIZE + 100, j as i32 * SQUARE_SIZE + 300, 50.0, Color::RED);}
-                else if field[i][j] == 2 {d.draw_circle(i as i32 * SQUARE_SIZE + 100, j as i32 * SQUARE_SIZE + 300, 50.0, Color::BLUE);} 
+                else if field[i][j] == 2 {d.draw_circle(i as i32 * SQUARE_SIZE + 100, j as i32 * SQUARE_SIZE + 300, 50.0, Color::BLUE);}
     	    }
         }
-    } 
+    }
 }
 
 fn get_field(d: &RaylibDrawHandle)-> i32 {
@@ -126,14 +122,14 @@ fn check_winner(field: [[i32; 3]; 3])-> i32 {
 			return field[0][i];
 		}
     }
-    	
+
     //Check vertical
     for i in 0..3 {
     	if field[i][0] != 0 && field[i][0] == field[i][1] && field[i][0] == field[i][2] {
     		return field[i][0];
     	}
     }
-    	
+
     //Check diagonal
     if field[0][0] != 0 && field[0][0] == field[1][1] && field[0][0] == field[2][2] {
     	return field[1][1];
@@ -149,7 +145,7 @@ fn check_winner(field: [[i32; 3]; 3])-> i32 {
     		}
     }
     if d {return 3;}
-    	   	
+
     return 0;
 }
 
