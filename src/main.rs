a31ac65045539c3f4c1ce06bb3d2759d2593939d
maxim-kruch <maximus2004.mk@gmail.com>
2022-07-31 19:19:21 +0200

    Fixed indentations

--- src/main.rs
+++ src/main.rs
@@ -22,44 +22,44 @@ fn main() {
 
     rl.set_target_fps(60);
 
-        //Main game loop
-	while !rl.window_should_close() {
+    //Main game loop
+    while !rl.window_should_close() {
 
         let mut d = rl.begin_drawing(&thread);
 
         //Get and interpret the input
         if d.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {
             let c_field: i32 = get_field(&d);
-        	println!("Pressed Field: {}", c_field);
+            println!("Pressed Field: {}", c_field);
 
-        	if c_field != 0 {
-        		if c_field == 1 && field[0][0] == 0 {field[0][0] = turn;}
-        		else if c_field == 2 && field[1][0] == 0 {field[1][0] = turn;}
-        		else if c_field == 3 && field[2][0] == 0 {field[2][0] = turn;}
-        		else if c_field == 4 && field[0][1] == 0 {field[0][1] = turn;}
-        		else if c_field == 5 && field[1][1] == 0 {field[1][1] = turn;}
-        		else if c_field == 6 && field[2][1] == 0 {field[2][1] = turn;}
-        		else if c_field == 7 && field[0][2] == 0 {field[0][2] = turn;}
-        		else if c_field == 8 && field[1][2] == 0 {field[1][2] = turn;}
-        		else if c_field == 9 && field[2][2] == 0 {field[2][2] = turn;}
+            if c_field != 0 {
+                if c_field == 1 && field[0][0] == 0 {field[0][0] = turn;}
+                else if c_field == 2 && field[1][0] == 0 {field[1][0] = turn;}
+                else if c_field == 3 && field[2][0] == 0 {field[2][0] = turn;}
+                else if c_field == 4 && field[0][1] == 0 {field[0][1] = turn;}
+                else if c_field == 5 && field[1][1] == 0 {field[1][1] = turn;}
+                else if c_field == 6 && field[2][1] == 0 {field[2][1] = turn;}
+                else if c_field == 7 && field[0][2] == 0 {field[0][2] = turn;}
+                else if c_field == 8 && field[1][2] == 0 {field[1][2] = turn;}
+                else if c_field == 9 && field[2][2] == 0 {field[2][2] = turn;}
 
-        		//Set the current turn to the next player
-                        if turn == 1 {turn = 2;} else {turn = 1;}
+                //Set the current turn to the next player
+                if turn == 1 {turn = 2;} else {turn = 1;}
 
-    			//Determine a winner and if found, reset the game
-                        if check_winner(field) != 0 {
-                            if check_winner(field) == 1 {
-                                println!("Player 1 wins!");
-                                p1_points += 1;
-                            } else if check_winner(field) == 2 {
-                                println!("Player 2 wins!");
-                                p2_points += 1;
-                            } else if check_winner(field) == 3 {
-                                println!("Draw!");
-                            }
-                            field = reset_game(field);
-                            turn = 1;
-                        }
+                //Determine a winner and if found, reset the game
+                if check_winner(field) != 0 {
+                    if check_winner(field) == 1 {
+                        println!("Player 1 wins!");
+                        p1_points += 1;
+                    } else if check_winner(field) == 2 {
+                        println!("Player 2 wins!");
+                        p2_points += 1;
+                    } else if check_winner(field) == 3 {
+                        println!("Draw!");
+                    }
+                        field = reset_game(field);
+                        turn = 1;
+                }
         	}
         }
 
@@ -91,7 +91,7 @@ fn main() {
 //Determine, where the cursor is, when a click is registered
 fn get_field(d: &RaylibDrawHandle)-> i32 {
 	if d.get_mouse_x() >= 0 && d.get_mouse_x() <= SQUARE_SIZE - 1 && d.get_mouse_y() >= SQUARE_SIZE + 1 && d.get_mouse_y() <= 2 * SQUARE_SIZE - 1 {
-		return 1;
+	    return 1;
 	} else if d.get_mouse_x() >= SQUARE_SIZE && d.get_mouse_x() <= 2 * SQUARE_SIZE - 1 && d.get_mouse_y() >= SQUARE_SIZE + 1 && d.get_mouse_y() <= 2 * SQUARE_SIZE - 1 {
 		return 2;
 	} else if d.get_mouse_x() >= 2 * SQUARE_SIZE && d.get_mouse_x() <= 3 * SQUARE_SIZE - 1 && d.get_mouse_y() >= SQUARE_SIZE + 1 && d.get_mouse_y() <= 2 * SQUARE_SIZE - 1 {
